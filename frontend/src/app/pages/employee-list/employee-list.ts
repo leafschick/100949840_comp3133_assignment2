1e7e775b4ea17bdfdb62da23db71e9c302fae314
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-04-02 18:06:35 -0400

    Created an employee list page UI with data

--- frontend/src/app/pages/employee-list/employee-list.ts
+++ frontend/src/app/pages/employee-list/employee-list.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './employee-list.html',
-  styleUrl: './employee-list.css',
+  styleUrls: ['./employee-list.css']
 })
-export class EmployeeList {}
+export class EmployeeList {
+ employees = [
+  {
+    id: 1,
+    firstName: 'Alice',
+    lastName: 'Johnson',
+    email: 'alice.johnson@coffeehouse.com',
+    department: 'Management',
+    position: 'Store Manager'
+  },
+  {
+    id: 2,
+    firstName: 'David',
+    lastName: 'Wilson',
+    email: 'david.wilson@coffeehouse.com',
+    department: 'Front of House',
+    position: 'Barista'
+  },
+  {
+    id: 3,
+    firstName: 'Emily',
+    lastName: 'Chen',
+    email: 'emily.chen@coffeehouse.com',
+    department: 'Kitchen',
+    position: 'Pastry Chef'
+  },
+  {
+    id: 4,
+    firstName: 'Michael',
+    lastName: 'Brown',
+    email: 'michael.brown@coffeehouse.com',
+    department: 'Operations',
+    position: 'Shift Supervisor'
+  }
+];
+}
