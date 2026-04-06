c4776132dc9d956bf02f39531ba8bef59eceb0c4
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-04-06 11:45:45 -0400

    Connect add employee page to backend

--- frontend/src/app/pages/add-employee/add-employee.ts
+++ frontend/src/app/pages/add-employee/add-employee.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './add-employee.html',
-  styleUrl: './add-employee.css',
+  styleUrls: ['./add-employee.css'],
 })
-export class AddEmployee {}
+export class AddEmployee {
+  firstName = '';
+  lastName = '';
+  email = '';
+  position = '';
+  department = '';
+  image = '';
+
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(private employeeService: EmployeeService, private router: Router) {}
+
+  addEmployee() {
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    if (!this.firstName || !this.lastName || !this.email || !this.position || !this.department) {
+      this.errorMessage = 'Please fill in all required fields.';
+      return;
+    }
+
+    this.employeeService.addEmployee({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      position: this.position,
+      department: this.department,
+      image: this.image
+    }).subscribe({
+      next: (response) => {
+        if (response.data?.addEmployee) {
+          this.successMessage = 'Employee added successfully!';
+
+          setTimeout(() => {
+            this.router.navigate(['/employees']);
+          }, 1000);
+        } else if (response.errors?.length) {
+          this.errorMessage = response.errors[0].message;
+        }
+      },
+      error: () => {
+        this.errorMessage = 'Failed to add employee.';
+      }
+    });
+  }
+}
