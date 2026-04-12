1656e7815e6b2eb5a602dba8badb07e73be2bf4a
Ayesha Akbar <ayesha.akbar6@gmail.com>
2026-04-12 10:58:06 -0400

    Removed company resource page and changed empolyee resource page to only work when users are logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,7 +7,6 @@ import { EmployeeList } from './pages/employee-list/employee-list';
 import { AddEmployee } from './pages/add-employee/add-employee';
 import { EditEmployee } from './pages/edit-employee/edit-employee';
 import { EmployeeDetail } from './pages/employee-detail/employee-detail';
-import { CompanyResources } from './pages/company-resources/company-resources';
 import { EmployeeResources } from './pages/employee-resources/employee-resources';
 import { TeamDirectory } from './pages/team-directory/team-directory';
 import { Onboarding } from './pages/onboarding/onboarding';
@@ -27,7 +26,6 @@ export const routes: Routes = [
   { path: 'add-employee', component: AddEmployee },
   { path: 'edit-employee/:id', component: EditEmployee },
   { path: 'employee/:id', component: EmployeeDetail },
-  { path: 'company-resources', component: CompanyResources },
   { path: 'employee-resources', component: EmployeeResources },
   {path: 'team-directory', component: TeamDirectory },
   {path: 'onboarding', component: Onboarding },
